dcbd2d72fb2bddbfbaed9765491295298e1cdb35
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-06-09 14:06:40 +0200

    wip

--- test/deprecated.jl
+++ test/deprecated.jl
@@ -22,53 +22,57 @@
 end
 
 @testset "stale_explicit_imports" begin
-    @test_logs match_mode = :any (:warn, r"deprecated") @test stale_explicit_imports(DynMod,
-                                                                                     "DynMod.jl") ==
-                                                              [DynMod => nothing,
-                                                               DynMod.Hidden => nothing]
+    # 3 dynamic include statements
+    l = (:warn, r"Dynamic")
+    logs = (l, l, l)
 
-    @test_logs match_mode = :any (:warn, r"deprecated") @test stale_explicit_imports_nonrecursive(DynMod,
-                                                                                                  "DynMod.jl") ===
-                                                              nothing
+    @test_logs (:warn, r"deprecated") logs... @test stale_explicit_imports(DynMod,
+                                                                           "DynMod.jl") ==
+                                                    [DynMod => nothing,
+                                                     DynMod.Hidden => nothing]
 
-    @test_logs match_mode = :any (:warn, r"deprecated") @test stale_explicit_imports(DynMod,
-                                                                                     "DynMod.jl";
-                                                                                     strict=false) ==
-                                                              [DynMod => [],
-                                                               # Wrong! Missing stale explicit export
-                                                               DynMod.Hidden => []]
+    @test_logs logs... (:warn, r"deprecated") @test stale_explicit_imports_nonrecursive(DynMod,
+                                                                                        "DynMod.jl") ===
+                                                    nothing
 
-    @test_logs match_mode = :any (:warn, r"deprecated") @test stale_explicit_imports_nonrecursive(DynMod,
-                                                                                                  "DynMod.jl";
-                                                                                                  strict=false) ==
-                                                              []
-    str = @test_logs match_mode = :any (:warn, r"deprecated") sprint(print_stale_explicit_imports,
-                                                                     DynMod,
-                                                                     "DynMod.jl")
+    @test_logs (:warn, r"deprecated") logs... @test stale_explicit_imports(DynMod,
+                                                                           "DynMod.jl";
+                                                                           strict=false) ==
+                                                    [DynMod => [],
+                                                     # Wrong! Missing stale explicit export
+                                                     DynMod.Hidden => []]
+
+    @test_logs logs... (:warn, r"deprecated") @test stale_explicit_imports_nonrecursive(DynMod,
+                                                                                        "DynMod.jl";
+                                                                                        strict=false) ==
+                                                    []
+    str = @test_logs (:warn, r"deprecated") logs... sprint(print_stale_explicit_imports,
+                                                           DynMod,
+                                                           "DynMod.jl")
     @test contains(str, "DynMod could not be accurately analyzed")
 
     # Printing via `print_stale_explicit_imports`
-    str = @test_logs match_mode = :any (:warn, r"deprecated") sprint(print_stale_explicit_imports,
-                                                                     TestModA,
-                                                                     "TestModA.jl")
+    str = @test_logs (:warn, r"deprecated") sprint(print_stale_explicit_imports,
+                                                   TestModA,
+                                                   "TestModA.jl")
     @test contains(str, "TestModA has no stale explicit imports")
     @test contains(str, "TestModC has stale explicit imports for these unused names")
 
     # Printing via `print_improper_qualified_accesses`
-    str = @test_logs match_mode = :any (:warn, r"deprecated") sprint(print_improper_qualified_accesses,
-                                                                     TestQualifiedAccess,
-                                                                     "test_qualified_access.jl")
+    str = @test_logs (:warn, r"deprecated") sprint(print_improper_qualified_accesses,
+                                                   TestQualifiedAccess,
+                                                   "test_qualified_access.jl")
     @test contains(str, "accesses 1 name from non-owner modules")
     @test contains(str, "`ABC` has owner")
 
-    @test_logs match_mode = :any (:warn, r"deprecated") @test only_name_source(stale_explicit_imports_nonrecursive(TestModA.SubModB.TestModA.TestModC,
-                                                                                                                   "TestModC.jl")) ==
-                                                              [(; name=:exported_c),
-                                                               (; name=:exported_d)]
+    @test_logs (:warn, r"deprecated") @test only_name_source(stale_explicit_imports_nonrecursive(TestModA.SubModB.TestModA.TestModC,
+                                                                                                 "TestModC.jl")) ==
+                                            [(; name=:exported_c),
+                                             (; name=:exported_d)]
 
     # Recursive version
-    lookup = @test_logs match_mode = :any (:warn, r"deprecated") Dict(only_name_source(stale_explicit_imports(TestModA,
-                                                                                                              "TestModA.jl")))
+    lookup = @test_logs (:warn, r"deprecated") Dict(only_name_source(stale_explicit_imports(TestModA,
+                                                                                            "TestModA.jl")))
     @test lookup[TestModA.SubModB.TestModA.TestModC] ==
           [(; name=:exported_c), (; name=:exported_d)]
     @test isempty(lookup[TestModA])
